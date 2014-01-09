4afb1a44a9d4a57d080bc613a84f74c06f620340
Andrew Or <andrewor14@gmail.com>
2014-01-08 17:59:35 -0800

    Get rid of weird offending log

--- core/src/main/scala/org/apache/spark/rdd/RDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/RDD.scala
@@ -711,7 +711,6 @@ abstract class RDD[T: ClassTag](
    */
   def count(): Long = {
     sc.runJob(this, (iter: Iterator[T]) => {
-      val _start = System.nanoTime()
       // Use a while loop to count the number of elements rather than iter.size because
       // iter.size uses a for loop, which is slightly slower in current version of Scala.
       var result = 0L
@@ -719,8 +718,7 @@ abstract class RDD[T: ClassTag](
         result += 1L
         iter.next()
       }
-      logWarning("### Counting took %s ns (%d elements)"
-        .format(System.nanoTime() - _start, result))
+      logWarning("weird...")
       result
     }).sum
   }
